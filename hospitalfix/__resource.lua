7f721c9270c3c33735ff2e96220f2180986fd7a2
JellieG <39775884+jellieg@users.noreply.github.com>
2020-11-30 19:35:10 +0100

    Update __resource.lua

--- hospitalfix/__resource.lua
+++ hospitalfix/__resource.lua
@@ -1,22 +1,3 @@
-server_scripts {
-  '@async/async.lua',
-  '@mysql-async/lib/MySQL.lua',
-  'server/main.lua'
-}
-
 client_scripts {
   'client/main.lua'
-}
-
-ui_page 'html/ui.html'
-files {
-  'html/ui.html',
-  'html/ui.css', 
-  'html/ui.js',
-  'html/close.png',
-  'html/shop.png',
-
-  'html/items/apple.png',
-  'html/items/water.png',
-  'html/items/bread.png',
 }
